d130595d1cb5f5d4858cf1389cee28f88ef49f96
raylou15 <86377551+raylou15@users.noreply.github.com>
2025-08-10 20:30:14 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   envDir: '../',
   server: {
     host: '0.0.0.0',
